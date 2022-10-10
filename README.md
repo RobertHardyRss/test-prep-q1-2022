57b7186ec8c1b6ab65224c6835446fe458387f1c
Robert Hardy <rhardy1@dcsdk12.org>
2022-10-10 11:50:45 -0600

    Add initial prep files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Intro to Video Game Development Test Prep
+
+Use this project to help you prepare for the test
+and to reinforce your familiarity with JavaScript concepts.
+
---
+++ scripts/app.js
@@ -0,0 +1,34 @@
+//@ts-check
+
+// Decalare a variable called "name" and assign your first name to it.
+
+// Log the variable you just created out to the console
+
+// change the following code so that "watermelon" is written to the console
+let fruit = "";
+console.log("The fruit variable equals: ", fruit);
+
+// Change the following code so the array is filtered to your
+// favorite fruit.  Feel free to add to the fruits array
+// if your favorite isn't in there.
+let fruits = ["grape", "apple", "kiwi", "strawberry"];
+fruits = fruits.filter((f) => "");
+console.log("The array of fruits now equals: ", fruits);
+
+// Create a new class called Player. Have the Player class
+// take in x and y coordinates as input parameters to it's
+// constructor.  Setup a property on the class named color,
+// and initialize the color property to a string value (you
+// can use a basic color or HSL string, your choice).
+//
+// Create a method on the class named update.  When update
+// is called, increment the x and y coordinates by 1.
+//
+// Create a display method.  When display is called, log the
+// x, y and color properties to the console.
+
+// declare a variable and set it to a new instance of Player
+
+// call the update method at least once
+
+// call the display method at least once
